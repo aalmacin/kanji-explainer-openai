de1d4b5ec63193a5706dde3907470758cb40fec5
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2023-05-07 19:53:33 -0600

    Initial commit

---
+++ src/main/java/com/raidrin/kanjiexplainer/KanjiExplainerApplication.java
@@ -0,0 +1,13 @@
+package com.raidrin.kanjiexplainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanjiExplainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanjiExplainerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raidrin/kanjiexplainer/KanjiExplainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raidrin.kanjiexplainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanjiExplainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
